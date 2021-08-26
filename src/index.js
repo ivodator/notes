a7a5bf3f274b714e91033aceb679d890286003f2
Ivo Dator <11011787+ivodator@users.noreply.github.com>
2021-08-26 12:07:11 -0700

    Authentication Update
    Updates:
    - /resolvers/mutation.js
    - /src/index.js
    - Added User to Resolver Context
    - Created sign-up and sign-in user flows

--- src/index.js
+++ src/index.js
@@ -7,6 +7,20 @@ const db = require('./db');
 const models = require('./models');
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
+const jwt = require('jsonwebtoken');
+
+// get the User info from a JWT
+const getUser = token => {
+    if (token) {
+        try {
+            // return the user information from the token
+            return jwt.verify(token, process.env.JWT_SECRET);
+        } catch (err) {
+            // if there's a problem with the token, throw an error
+            throw new Error('Session invalid');
+        }
+    }
+};
 
 // Run the server on a port specified in our .env file or port 4000
 const port = process.env.PORT || 4000;
@@ -21,7 +35,13 @@ db.connect(DB_HOST);
 const server = new ApolloServer({ 
     typeDefs, 
     resolvers,
-    context: () => {
+    context: ({ req }) => {
+        // Get the user token from the headers
+        const token = req.headers.authorization;
+        // try to retrieve a user with the token
+        const user = getUser(token);
+        // for now, let's log the user to the console:
+        console.log(user);
         // Add the db models to the context
         return { models };
     } 
