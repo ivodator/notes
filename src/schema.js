38d5c0ab922bb83346c9c05a613a91870e62ebda
Ivo Dator <11011787+ivodator@users.noreply.github.com>
2021-08-26 11:36:58 -0700

    User Accounts and Authentication Update
    Added:
    - Authentication Flow
    - Encryption
    - Integrated authentication into API

--- src/schema.js
+++ src/schema.js
@@ -2,10 +2,21 @@ const { gql } = require('apollo-server-express');
 
 // Construct a schema, using GraphQL schema language
 module.exports = gql`
+    scalar DateTime
     type Note {
         id: ID!
         content: String!
-        author: String!
+        author: User!
+        createdAt: DateTime!
+        updatedAt: DateTime!
+    }
+
+    type User {
+        id: ID!
+        username: String!
+        email: String!
+        avatar: String
+        notes: [Note!]!
     }
 
     type Query {
@@ -18,6 +29,8 @@ module.exports = gql`
         newNote(content: String!): Note!
         updateNote (id: ID!, content: String!): Note!
         deleteNote (id: ID!): Boolean!
+        signUp(username: String!, email: String!, password: String!): String!
+        signIn(username: String, email: String, password: String!): String!
     }
 `;
 
